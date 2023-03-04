2a4dbe39562d114e0003adb614cf5a57da9d0123
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2023-03-04 17:11:41 +0000

    rhubarb-geek-nz

--- test.cs
+++ test.cs
@@ -2,7 +2,7 @@
  *
  *  Copyright 2022, Roger Brown
  *
- *  This file is part of rhubarb-geek-pi/SQLite.Interop
+ *  This file is part of rhubarb-geek-nz/SQLite.Interop
  *
  *  This program is free software: you can redistribute it and/or modify it
  *  under the terms of the GNU Lesser General Public License as published by the
